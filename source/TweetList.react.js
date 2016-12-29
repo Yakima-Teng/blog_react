6c375e5bb3bc07bbac225434cde7e4f39a34579c
Yakima Teng <cleveryun@163.com>
2016-12-29 21:49:29 +0800

    removes some redundant files

--- source/TweetList.react.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 4/13.
- */
-var React = require('react');
-var Tweet = require('./Tweet.react.js');
--- source/utils/TweetUtils.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 4/14.
- */
-function getListOfTweetIds(tweets) {
-    return Object.keys(tweets);
-}
-module.exports.getListOfTweetIds = getListOfTweetIds;
